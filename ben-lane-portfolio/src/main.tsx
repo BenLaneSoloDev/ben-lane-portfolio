2e46d7dce062d069add14be5494e610eaf02ee05
Ben-Lane <benjaminkieranlane@gmail.com>
2026-07-27 15:38:55 +0100

    feat: Installed Tailwind CSS

--- ben-lane-portfolio/src/main.tsx
+++ ben-lane-portfolio/src/main.tsx
@@ -5,8 +5,6 @@ import './index.css'
 import { RouterProvider } from "react-router/dom";
 import { router } from './router.tsx';
 
-
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
