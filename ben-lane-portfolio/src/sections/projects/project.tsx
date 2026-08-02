85b870ee9432c715bd322051c07a6c2c3a864005
Ben-Lane <benjaminkieranlane@gmail.com>
2026-08-02 21:04:02 +0100

    fix: Altered Project Cards For Grid

--- ben-lane-portfolio/src/sections/projects/project.tsx
+++ ben-lane-portfolio/src/sections/projects/project.tsx
@@ -22,23 +22,19 @@ export default function Project({activeTags, project}: {activeTags: string[], pr
   const validStatus = ["ongoing", "paused", "completed"];
 
   return (
-    <div className="flex flex-row flex-wrap w-[45%] items-center p-6 gap-6 rounded-4xl font-light text-def-grey bg-def-white border-3 border-def-orange shadow-md/20">
-      <div className="flex flex-col flex-1 gap-2">
-        <div className="">
-          <h2 className="ml-2 text-lg">
-            <strong className="text-xl">{title}</strong> | {subtitle}
-          </h2>
+    <div className="flex flex-col overflow-hidden w-[45%] items-center rounded-4xl font-light text-def-grey bg-def-white border-4 border-def-orange shadow-md/20">
+      <div className="w-full relative">
+        <div className="flex flex-col absolute gap-3 bottom-3 right-3">
+          {githubLink && <Button className={`${buttonStyles["button-s"]} ${buttonStyles.highlight} p-5 text-base font-semibold text-def-grey border-def-green border-2`} variant="outline" nativeButton={false} render={<a href={githubLink} target="_blank"></a>}>Github</Button>}
+          {liveLink && <Button className={`${buttonStyles["button-s"]} ${buttonStyles.highlight} p-5 text-base font-semibold text-def-grey border-def-green border-2`} variant="outline" nativeButton={false} render={<a href={liveLink} target="_blank"></a>}>Try It Out</Button>} 
         </div>
-        <div className="w-35">
-          <img src={`./src/assets/home/projectSS/${poster}`} alt="Project Screenshot"></img>
-        </div>
+        <img className="object-cover object-center aspect-video h-full" src={`./src/assets/home/projectSS/${poster}`} alt="Project Screenshot"></img>
       </div>
-      <div className="flex flex-col flex-1 gap-4 text-base">
-        <p>{briefDescription}</p>
-        <div className="flex flex-row gap-4 items-center">
-          {githubLink && <Button className={`${buttonStyles.button} ${buttonStyles.highlight} p-5 text-base font-semibold text-def-grey border-def-green border-2`} variant="outline" nativeButton={false} render={<a href={githubLink} target="_blank"></a>}>Github</Button>}
-          {liveLink && <Button className={`${buttonStyles.button} ${buttonStyles.highlight} p-5 text-base font-semibold text-def-grey border-def-green border-2`} variant="outline" nativeButton={false} render={<a href={liveLink} target="_blank"></a>}>Try It Out</Button>} 
-        </div>
+      <div className="flex flex-col p-3 gap-3 border-t-4 border-def-orange"> 
+        <h2 className="w-full text-lg">
+          <strong className="text-xl">{title}</strong> | {subtitle}
+        </h2>
+        <p className="mx-4 mb-1">{briefDescription}</p>
         <div className="flex flex-row flex-wrap gap-1 items-center">
           <Badge className={`${buttonStyles.highlight} font-normal`}>{validStatus.some(s => s === status.toLowerCase()) ? capitaliseString(status) : "Completed"}</Badge>
           <span className="drop-shadow-subtle">|</span>
@@ -46,7 +42,7 @@ export default function Project({activeTags, project}: {activeTags: string[], pr
             <Badge key={`t${index}`} className={(activeTags.includes(tag) ? `${buttonStyles.highlightToggle}` : `${buttonStyles.highlight}`) + ` font-normal text-def-grey`} variant="outline">{capitaliseString(tag)}</Badge>
           ))}
         </div>
-      </div>
+      </div>      
     </div>
   );
 }
