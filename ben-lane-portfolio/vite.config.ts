34bd12aa232a902a6721a53d8cf14e191fedd716
Ben-Lane <benjaminkieranlane@gmail.com>
2026-08-06 14:26:04 +0100

    feat: Setup Vite Base Config (for deployment)

--- ben-lane-portfolio/vite.config.ts
+++ ben-lane-portfolio/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  base: process.env.VITE_BASE_PATH || "/ben-lane-portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
